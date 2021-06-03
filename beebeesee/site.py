ba8300c5c9242f5d566ae9b2c5639771997e0487
stonkmem <73430589+stonkmem@users.noreply.github.com>
2021-06-03 23:53:27 +0800

    Update site.py

--- beebeesee/site.py
+++ beebeesee/site.py
@@ -8,84 +8,24 @@ from .info import PRE_PATH, PROJECT_NAME, STATIC_DIR, VIEWS_DIR, UPLOAD_DIR
 
 site = Blueprint("site", PROJECT_NAME, template_folder=VIEWS_DIR)
 
+camera = cv2.
 
-@site.route("/", methods=["GET"])
-def root():
-    return render_template("root.html", pre_path=PRE_PATH)
-
-
-@site.route("/success", methods=["GET"])
-def success():
-    return render_template("success.html")
-
-
-"""
-@site.route("/testing", methods=["GET"])
-def testing():
-    return render_template("pullmyhair.html")
-
-
-@site.route('/signup', methods=['GET','POST'])
-def signup():
-    if request.method == 'POST':
-        print(request.form)
-        return jsonify(request.form['userID'], request.form['file'])
-    return render_template('signup.html')
-"""
-
-
-@site.route("/analyzer", methods=['GET', 'POST'])
-def analyzer():
-    ALLOWED_EXTS: Set[str] = {".png", ".jpg", ".jpeg"}
-    def file_check(filename: str) -> bool:
-        p = Path(filename)
-        return p.suffix in ALLOWED_EXTS
-    if request.method == "POST":
-        print(request)
-        print(request.files)
-        if "myfile" not in request.files:
-            print("myfile not in request.files")
-            flash("No file part.")
-            return redirect(request.url)
-        file = request.files["myfile"]
-        if file.filename == "":
-            print("file.filename == ''")
-            flash("No selected file.")
-            return redirect(request.url)
-        ok = file_check(file.filename)
-        print(file, ok)
-        if file and file_check(file.filename):
-            print("file and file_check(file.filename)")
-            filename = secure_filename(file.filename)
-            file.save(UPLOAD_DIR / filename)
-            return redirect(url_for("site.success"))
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
         else:
-            return redirect(request.url)
-    elif request.method == "GET":
-        return render_template("analyzer.html")
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+            
+@app.route('/video_feed')
+def video_feed():
+    return <img src = Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')>
 
-
-
-@site.route("/__backdoor/videofeed", methods=["POST"])
-def backdoor_videofeed():
-    print(request.form["file"])
-
-
-@site.route("/_model/model", methods=["GET"])
-def _model_model():
-    MODEL_PATH = STATIC_DIR / "model" / "model.json"
-    model = json.load(MODEL_PATH.open("r"))
-    return model
-
-
-@site.route("/_model/<filename>", methods=["GET"])
-def _model_file(filename):
-    filepath = STATIC_DIR / "model" / filename
-    with filepath.open("rb") as f:
-        data = f.read()
-    return data
-
-
+            
 @site.route("/<lost>/")
 def lostpage(lost):
     return """
